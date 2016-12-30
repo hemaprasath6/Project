5b94b0fa79f0832cda70b60dddbe3081f95d3456
Hemaprasath <jinaprasath6@gmail.com>
2016-12-30 12:19:50 +0530

    Add files via upload

---
+++ techshoppie/src/main/java/com/niit/Controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.niit.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/about")
+	public String showAboutUs()
+	{
+		return "aboutus";
+	}
+	
+	@RequestMapping("/registration")
+	public String showregistration()
+	{
+		return "registration";
+	}
+
+	@RequestMapping("/login")
+	public String showlogin()
+	{
+		return "login";
+	}
+}
+
